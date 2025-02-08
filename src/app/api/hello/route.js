f5d9702aab246b6668807e8809ed858d950a66c2
mishi54 <mishi121006@gmail.com>
2025-02-08 15:09:37 -0700

    m

--- src/app/api/hello/route.js
+++
@@ -1,7 +0,0 @@
-export function GET(req) {
-    return new Response(JSON.stringify({ message: 'Hello from Next.js!' }), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 200,
-    });
-  }
-  
